5becc57f8448e38bd9f5cf917a8693e4fca3b071
excitedstate <44634316+excitedstate@users.noreply.github.com>
2019-08-22 00:11:32 +0800

    Update update_db_company.py

--- func/update_db_company.py
+++ func/update_db_company.py
@@ -26,30 +26,6 @@ def update_db_company():
         sql_query = '''update sak_news set company="%s" where title='%s' '''%(str(company),title)
         db.exec_sql_query(sql_query)
 
-def update_db_company_backup():
-    now_date = get_today_date()
-    with open('src/stock_code.txt','r',encoding='utf-8')as f:
-        lines = f.readlines()
-    com_code = {}
-    for line in lines:
-        tmp = line.strip().split(',')
-        com_code[tmp[1]] = tmp[0]
-    db = SAKSqlDBS()
-    sql_query = '''select title,content from sak_news where date='%s';''' %now_date
-    res = db.exec_sql_query(sql_query)
-    test_dic = {ele[0]:ele[1] for ele in res}
-    dic_all = {}
-    for title,content in test_dic.items():
-        tmp_content = []
-        for com in com_code.keys():
-            if com in content:
-                tmp_content.append(com)
-        dic_all[title] = tmp_content
-    for title, company in dic_all.items():
-        print(title, company)
-        sql_query = '''update sak_news set company="%s" where title='%s' '''%(str(company),title)
-        db.exec_sql_query(sql_query)
-
 # # 已完成历史使命的函数
 def get_symbol_name():
     db = SAKSqlDBS()
